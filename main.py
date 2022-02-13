4bd4b06f065c3070822049b6c73ee5cbe9404f24
tejas <tejasmalo10@gmail.com>
2022-02-13 10:56:43 +0530

    first recursion program

--- main.py
+++
@@ -1,8 +0,0 @@
-def fun1(n):
-	i = 0
-	if (n > 1):
-		fun1(n - 1)
-	for i in range(n):
-		print(" * ",end="")
-
-# This code is contributed by shubhamsingh10
--- sum of digits/sumofDIgits.py
+++ sum of digits/sumofDIgits.py
@@ -3,13 +3,14 @@
 
 # Function to check sum of
 # digit using recursion
-def sum_of_digit(n):
-    if n == 0:
-        return 0
-    return (n % 10 + sum_of_digit(int(n / 10)))
-
+def sum_of_digit( n ):
+	if n == 0:
+		return 0
+	return (n % 10 + sum_of_digit(int(n / 10)))
 
 # Driven code to check above
 num = 12345
 result = sum_of_digit(num)
-print("Sum of digits in", num, "is", result)
+print("Sum of digits in",num,"is", result)
+
+# This code is contributed by "Sharad_Bhardwaj".
